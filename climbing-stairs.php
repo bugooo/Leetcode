1b6a2cd09046db9091419ac9dd21b99e88df0c1e
bugooo <2769456918@qq.com>
2020-04-07 20:51:53 +0800

    146

--- climbing-stairs.php
+++ climbing-stairs.php
@@ -5,7 +5,7 @@ class Solution {
      * @param Integer $n
      * @return Integer
      */
-    //递归 F(n)= F(n-1)+F(n-2)
+    //递归 F(n)= F(n-1)+F(n-2) 用数组记录历史数据 减少递归中的重复计算
     function climbStairs($n){
         if(array_key_exists($n,$this->climbStairs)){
             return $this->climbStairs[$n];
