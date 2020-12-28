6075ce7c2fcbd4251532ab1a3d44154d75e75569
bugooo <2769456918@qq.com>
2020-12-28 17:16:50 +0800

    189

--- PHP/189.旋转数组.php
+++ PHP/189.旋转数组.php
@@ -13,6 +13,22 @@ class Solution {
      * @return NULL
      */
     function rotate(&$nums, $k) {
+       $len  = count($nums);
+       $k = $k % $len;
+       $count = 0;         // 记录交换位置的次数，n个同学一共需要换n次
+        for($start = 0; $count < $len; $start++) {
+            $cur = $start;       // 从0位置开始换位子
+            $pre = $nums[$cur];   
+            do{
+                $next = ($cur + $k) % $len;
+                $temp = $nums[$next];    // 来到角落...
+                $nums[$next] = $pre;
+                $pre = $temp;
+                $cur = $next;
+                $count++;
+            }while($start != $cur)  ;     // 循环暂停，回到起始位置，角落无人
+             
+        }
 
     }
 }
