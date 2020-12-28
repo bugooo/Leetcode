66f4803422d43db3fd7dd5f9b891dcea62574e6b
bugooo <2769456918@qq.com>
2020-12-28 17:16:15 +0800

    189

---
+++ PHP/189.旋转数组.php
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=php
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+
+    }
+}
+// @lc code=end
+
